88e1350d48156bb672c432e66a3c3432b1702293
VladimirElman <daniel_vov@mail.ru>
2021-01-24 19:14:41 +0200

    в

--- src/main/java/day4/Task1.java
+++ src/main/java/day4/Task1.java
@@ -1,7 +1,45 @@
 package day4;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        int sum = 0;
+        int even = 0;
+        int odd = 0;
+        int a = 0;
+        int b = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = (int) (Math.random() * 10);
+            System.out.print(numbers[i] + " ");
+            sum = sum + numbers[i];
+            if (numbers[i] % 2 == 0) {
+                even++;
+            }
+            if (numbers[i] % 2 - 1 == 0) {
+                odd++;
+            }
+            if (numbers[i] == 1) {
+                a++;
+            }
+            if (numbers[i] > 8) {
+                b++;
+            }
+        }
+        System.out.println();
+        System.out.println("Информация о массиве:");
+        System.out.println("Длина массива: " + numbers.length);
+        System.out.println("Количество чисел больше восьми: " + b);
+        System.out.println("Количество чисел равных единице: " + a);
+        System.out.println("Количетсво четных чисел: " + even);
+        System.out.println("Количество нечетных чисел: " + odd);
+        System.out.println("Сумма всех элементов массива: " + sum);
+
 
     }
 }
+
--- src/main/java/day4/Task2.java
+++ src/main/java/day4/Task2.java
@@ -1,7 +1,45 @@
 package day4;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] myArray = new int[100];
+        Random random = new Random();
+        for (int i = 0; i < myArray.length; i++)
+            myArray[i] = random.nextInt(10000);
+        int max = 0;
+        for (int x : myArray) {
+            if (x > max) {
+                max = x;
+            }
+        }
+        System.out.println("Максимальное число в массиве: " + max);
+        int min = 10000;
+        for (int x : myArray) {
+            if (x < min) {
+                min = x;
+            }
+        }
+        System.out.println("Минимальное число в массиве: " + min);
+        int counter = 0;
+        for (int x : myArray) {
+            if (x % 10 == 0)
+                counter++;
+
+        }
+        System.out.println("Количество чисел оканчивающихся на 0:" + counter);
+        int sum = 0;
+        for (int x : myArray) {
+            if (x % 10 == 0)
+                counter++;
+            sum += x;
+        }
+        System.out.println("Сумма элементов оканчивающихся на 0: " + sum);
+
 
     }
 }
+
